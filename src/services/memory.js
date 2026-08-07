eb7d4fb939cbc59ca387c89518ef0f88293c60bf
shilohmtc <shilohmtc@gmail.com>
2026-08-07 12:40:44 +0200

    Update memory.js

--- src/services/memory.js
+++ src/services/memory.js
@@ -1,1 +1,19 @@
+const sessions = new Map();
 
+function getSession(phone) {
+  return sessions.get(phone);
+}
+
+function saveSession(phone, responseId) {
+  sessions.set(phone, responseId);
+}
+
+function clearSession(phone) {
+  sessions.delete(phone);
+}
+
+module.exports = {
+  getSession,
+  saveSession,
+  clearSession,
+};
