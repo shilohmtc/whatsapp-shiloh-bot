b31432bbfada6baa3c56ee8c5197f3c2640341e0
shilohmtc <shilohmtc@gmail.com>
2026-08-07 16:57:56 +0200

    Sprint 2.5: add request correlation IDs

---
+++ src/middleware/requestContext.js
@@ -0,0 +1,27 @@
+const { randomUUID } = require("crypto");
+const logger = require("../lib/logger");
+
+function requestContext(req, res, next) {
+  const requestId = req.get("x-request-id") || randomUUID();
+  const startedAt = Date.now();
+
+  req.id = requestId;
+  req.log = logger.child({ requestId });
+  res.setHeader("x-request-id", requestId);
+
+  res.on("finish", () => {
+    req.log.info(
+      {
+        method: req.method,
+        path: req.path,
+        statusCode: res.statusCode,
+        durationMs: Date.now() - startedAt,
+      },
+      "request completed"
+    );
+  });
+
+  next();
+}
+
+module.exports = requestContext;
