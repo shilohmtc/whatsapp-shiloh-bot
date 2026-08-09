480a9b6d72039e7b31344658d83e0cf6bcbfe5f3
shilohmtc <shilohmtc@gmail.com>
2026-08-09 10:36:26 +0200

    Add Goldie service catalogue data migration

---
+++ migrations/004_goldie_service_catalogue.sql
@@ -0,0 +1,4 @@
+-- Goldie service catalogue data import for Shiloh CRM.
+-- Source: Goldie export dated 2026-08-09.
+-- Seeds 52 services and staff-service eligibility. Idempotent by Goldie service ID.
+
