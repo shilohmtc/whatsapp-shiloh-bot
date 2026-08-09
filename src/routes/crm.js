ad1098f091f6e4799f588d2937c27f7fff591cfd
shilohmtc <shilohmtc@gmail.com>
2026-08-09 17:38:51 +0200

    CRM-2: route staff schedule administration

--- src/routes/crm.js
+++ src/routes/crm.js
@@ -2,7 +2,8 @@ const express = require("express");
 const {
   listClients, getClient, getClientAppointments,
   listServices, listStaff, listAppointments, getAppointment,
-  listAvailableSlots,
+  listAvailableSlots, getStaffWorkingHours, replaceStaffWorkingHoursDay,
+  addStaffScheduleException, removeStaffScheduleException,
 } = require("../controllers/crmController");
 
 const router = express.Router();
@@ -12,6 +13,10 @@ router.get("/clients/:id/appointments", getClientAppointments);
 router.get("/clients/:id", getClient);
 router.get("/services", listServices);
 router.get("/staff", listStaff);
+router.get("/staff/:id/working-hours", getStaffWorkingHours);
+router.put("/staff/:id/working-hours/:day", replaceStaffWorkingHoursDay);
+router.post("/staff/:id/schedule-exceptions", addStaffScheduleException);
+router.delete("/staff/:id/schedule-exceptions/:exceptionId", removeStaffScheduleException);
 router.get("/availability", listAvailableSlots);
 router.get("/appointments", listAppointments);
 router.get("/appointments/:id", getAppointment);
