26eae86bc6bb47987f57f866be25728a5d2bf29b
shilohmtc <shilohmtc@gmail.com>
2026-08-09 10:36:44 +0200

    Remove incomplete Goldie service catalogue migration

--- migrations/004_goldie_service_catalogue.sql
+++
@@ -1,4 +0,0 @@
--- Goldie service catalogue data import for Shiloh CRM.
--- Source: Goldie export dated 2026-08-09.
--- Seeds 52 services and staff-service eligibility. Idempotent by Goldie service ID.
-
