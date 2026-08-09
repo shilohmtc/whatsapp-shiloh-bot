f40f4c883db5d0d088b8235ef709fdfb25c4289e
shilohmtc <shilohmtc@gmail.com>
2026-08-09 10:14:39 +0200

    Use shared PostgreSQL pool for database inspection

--- src/services/databaseInspector.js
+++ src/services/databaseInspector.js
@@ -1,16 +1,4 @@
-const { Pool } = require("pg");
-const logger = require("../lib/logger");
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL?.includes("render.com")
-    ? { rejectUnauthorized: false }
-    : undefined,
-});
-
-pool.on("error", (error) => {
-  logger.error({ err: error }, "unexpected database inspector PostgreSQL pool error");
-});
+const { pool } = require("../db/pool");
 
 async function getDatabaseStatus() {
   const result = await pool.query(`
