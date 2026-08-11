54808ca378b93745f9a0e7d498b85cd4472c0184
shilohmtc <shilohmtc@gmail.com>
2026-08-11 19:11:15 +0200

    Cleanup: remove completed loyalty migration hook (#71)
    Remove the temporary pre-listen migration pass after migration 040 was successfully applied and production health verified.

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const walkinRoutes = require("./src/routes/walkin");
 const bookRoutes = require("./src/routes/book");
 const serviceRoutes = require("./src/routes/services");
 const { checkDatabase } = require("./src/services/memory");
-const { applyPendingMigrations } = require("./src/services/migrations");
 const { startGoogleBusinessProfileSyncScheduler } = require("./src/services/googleBusinessProfileSync");
 const { startAppointmentLifecycleScheduler } = require("./src/services/appointmentLifecycle");
 const { startCustomerCareScheduler } = require("./src/services/customerCare");
@@ -50,8 +49,6 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 3000;
 let server;
 async function start() {
-  const migrationResult = await applyPendingMigrations();
-  logger.info({ migrationResult }, "Guarded loyalty migration pass completed");
   server = app.listen(PORT, () => {
     logger.info({ port: PORT }, "Shiloh started");
     startGoogleBusinessProfileSyncScheduler();
