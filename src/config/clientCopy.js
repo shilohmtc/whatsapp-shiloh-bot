c53455da82e162c49986de0f5eb44948b4c88a3b
shilohmtc <shilohmtc@gmail.com>
2026-08-13 19:42:36 +0200

    Add safe client-facing copy config surface

---
+++ src/config/clientCopy.js
@@ -0,0 +1,5 @@
+const CLIENT_COPY = Object.freeze({
+  bookingDiscoveryPrompt: 'Choose a service below and I’ll show you the available treatments and practitioners. 🌿',
+});
+
+module.exports = { CLIENT_COPY };
