40cb006a7cf318dcfc965f4f8fd89d9c42617943
shilohmtc <shilohmtc@gmail.com>
2026-08-20 21:21:31 +0200

    Preload multi-staff client cancellation safety

--- src/bootstrap/clientCouplesPackagesPatch.js
+++ src/bootstrap/clientCouplesPackagesPatch.js
@@ -1,3 +1,4 @@
+require('./clientMultiStaffAppointmentChangePatch');
 const discovery = require('../services/clientDiscoveryPackages');
 const couplesBooking = require('../services/clientCouplesMassageBooking');
 
