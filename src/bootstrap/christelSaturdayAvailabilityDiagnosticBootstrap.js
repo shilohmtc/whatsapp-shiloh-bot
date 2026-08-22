e4a30ce833b2f73d32c03ea9e3c2009ee4a04ab4
shilohmtc <shilohmtc@gmail.com>
2026-08-22 12:06:49 +0200

    Remove temporary availability diagnostic bootstrap

--- src/bootstrap/christelSaturdayAvailabilityDiagnosticBootstrap.js
+++
@@ -1,8 +0,0 @@
-const logger = require('../lib/logger');
-const { runChristelSaturdayAvailabilityDiagnostic } = require('../services/christelSaturdayAvailabilityDiagnostic');
-
-setImmediate(() => {
-  runChristelSaturdayAvailabilityDiagnostic().catch((error) => {
-    logger.error({ err: error }, 'Christel Saturday availability diagnostic failed');
-  });
-});
