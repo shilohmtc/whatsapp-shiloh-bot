38fabb1ccd0b4618ea4731ea2c12ad5657f354c7
shilohmtc <shilohmtc@gmail.com>
2026-09-02 13:13:23 +0200

    feat(workspace): mount bounded client notification routes

--- src/routes/calendar.js
+++ src/routes/calendar.js
@@ -10,6 +10,7 @@ const { createOptionalCalendarSessionMiddleware } = require('../middleware/staff
 const { createOperatorContactAuthorityRouter } = require('./operatorContactAuthority');
 const { createCalendarOperationalMutationRouter } = require('./calendarOperationalMutations');
 const { createWorkspaceClientsRouter } = require('./workspaceClients');
+const { createWorkspaceClientNotificationRouter } = require('./workspaceClientNotifications');
 const { createWorkspaceStaffRouter } = require('./workspaceStaff');
 const { createWorkspaceStaffMutationRouter } = require('./workspaceStaffMutations');
 const { createWorkspaceServicesRouter } = require('./workspaceServices');
@@ -47,6 +48,7 @@ router.use('/client-authority', createOperatorContactAuthorityRouter({ sessionSe
 router.use('/book', createCalendarCreateBookingRouter({ sessionService: staffBrowserSessionService }));
 router.use('/operations', createCalendarOperationalMutationRouter({ sessionService: staffBrowserSessionService }));
 router.use('/clients', createWorkspaceClientsRouter({ sessionService: staffBrowserSessionService }));
+router.use('/clients', createWorkspaceClientNotificationRouter({ sessionService: staffBrowserSessionService }));
 router.use('/team', createWorkspaceStaffRouter({ sessionService: staffBrowserSessionService }));
 router.use('/team', createWorkspaceStaffMutationRouter({ sessionService: staffBrowserSessionService }));
 router.use('/services', createWorkspaceServicesRouter({ sessionService: staffBrowserSessionService }));
