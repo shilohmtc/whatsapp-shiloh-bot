e6d5fffeb226d4774539e138d23f51a06ec806f4
shilohmtc <shilohmtc@gmail.com>
2026-09-09 16:06:53 +0200

    #794 mount passkey auth boundary

--- src/routes/calendar.js
+++ src/routes/calendar.js
@@ -7,6 +7,7 @@ const { createCalendarRetrospectiveBookingRouter } = require('./calendarRetrospe
 const { createStaffBrowserSessionService } = require('../services/staffBrowserSession');
 const { createStaffBrowserSessionRouter } = require('./staffBrowserSession');
 const { createStaffAuthBrowserEnrollmentRouter } = require('./staffAuthBrowserEnrollment');
+const { createStaffPasskeyAuthRouter } = require('./staffPasskeyAuth');
 const { createOptionalCalendarSessionMiddleware } = require('../middleware/staffBrowserSession');
 const { createOperatorContactAuthorityRouter } = require('./operatorContactAuthority');
 const { createCalendarOperationalMutationRouter } = require('./calendarOperationalMutations');
@@ -51,6 +52,7 @@ router.get('/:token.ics',async(req,res,next)=>{try{
 router.use(createWorkspacePwaHtmlMiddleware());
 router.use('/pwa', createWorkspacePwaRouter({ sessionService: staffBrowserSessionService }));
 router.use('/staff-auth/admin-enrollment', createStaffAuthBrowserEnrollmentRouter({ sessionService: staffBrowserSessionService }));
+router.use('/staff-auth/passkeys', createStaffPasskeyAuthRouter({ sessionService: staffBrowserSessionService }));
 router.use('/staff-auth', createStaffBrowserSessionRouter({ service: staffBrowserSessionService }));
 router.use('/staff', staffCalendarAccessUxRoutes);
 router.use('/client-authority', createOperatorContactAuthorityRouter({ sessionService: staffBrowserSessionService }));
