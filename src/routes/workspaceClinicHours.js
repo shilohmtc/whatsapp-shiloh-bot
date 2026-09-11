4943d2812407f30c1159d11ab04918b9b2f3c7f8
shilohmtc <shilohmtc@gmail.com>
2026-09-11 15:37:01 +0200

    fix #877 enforce loaded ZA holiday guard

--- src/routes/workspaceClinicHours.js
+++ src/routes/workspaceClinicHours.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const workspaceClinicHours = require('../services/workspaceClinicHours');
+const workspaceClinicHoursHolidayGuard = require('../services/workspaceClinicHoursHolidayGuard');
 const {
   renderClinicHoursPage,
   clinicHoursClientScript,
@@ -43,6 +44,7 @@ function createWorkspaceClinicHoursRouter({
   env = process.env,
   sessionService,
   service = workspaceClinicHours,
+  holidayGuard = workspaceClinicHoursHolidayGuard,
   renderPage = renderClinicHoursPage,
 } = {}) {
   if (!sessionService) throw new Error('Workspace Clinic hours routes require the existing staff browser session service');
@@ -89,6 +91,7 @@ function createWorkspaceClinicHoursRouter({
 
   router.post('/exceptions', sameOrigin, requireCsrf, async (req, res, next) => {
     try {
+      await holidayGuard.requireLoadedZaPublicHoliday(req.body?.exceptionDate);
       const result = await service.upsertException({
         adminId: req.staffBrowserSession?.adminId,
         exceptionDate: req.body?.exceptionDate,
