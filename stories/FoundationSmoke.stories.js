7d52fb15eff2ce3d6f0ee813c2e9f7d178717511
shilohmtc <shilohmtc@gmail.com>
2026-09-11 10:24:51 +0200

    #865 remove Storybook bootstrap smoke story

--- stories/FoundationSmoke.stories.js
+++
@@ -1,7 +0,0 @@
-export default {
-  title: 'Foundations/Storybook Smoke',
-};
-
-export const Ready = {
-  render: () => '<div style="font:16px system-ui;padding:24px">Shiloh Storybook HTML/Vite workshop ready</div>',
-};
